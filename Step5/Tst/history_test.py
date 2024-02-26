2b88708499ba779ead836253ad44a836456dfb12
Volovikov Alexander <avolovikov@it-one.ru>
2024-02-26 05:02:47 +0300

    fix

--- Step5/Tst/history_test.py
+++ Step5/Tst/history_test.py
@@ -1,5 +1,5 @@
 import random
-from Src.Models.nomenclature_history import nomenclature_hostory
+from Src.Models.nomenclature_history import nomenclature_history
 import unittest
 import uuid
 from random import randrange
@@ -17,13 +17,13 @@ class history_test(unittest.TestCase):
         random_second = randrange(int_delta)
         return start + timedelta(seconds=random_second)
     
-    def get_nomenclature_hostory():
+    def get_nomenclature_history():
         "Сформировать набор исторических данных "
         items = []
         keys = ["203be59a-926f-11ee-b9d1-0242ac120002" , "53728856-926f-11ee-b9d1-0242ac120002", "79c9f71e-926f-11ee-b9d1-0242ac120002", "9f1fed2c-926d-11ee-b9d1-0242ac120002" ]
         
         for nomenclature_code in keys:
-            item = nomenclature_hostory()
+            item = nomenclature_history()
             item.nomenclature_code = uuid.UUID(nomenclature_code)
             item.period = history_test.random_date(datetime.strptime("2023-12-01", "%Y-%m-%d"),
                                                        datetime.strptime("2024-01-01", "%Y-%m-%d"))
@@ -39,7 +39,7 @@ class history_test(unittest.TestCase):
         # Подготовка
         
         # Действия
-        items =  history_test.get_nomenclature_hostory()
+        items =  history_test.get_nomenclature_history()
               
         # Проверки
         assert len(items) > 0    
