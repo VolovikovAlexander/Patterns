d0606805cf5695328af80d2d84849477debde6fe
Volovikov Alexander <avolovikov@it-one.ru>
2024-03-25 15:22:14 +0300

    fix

--- Result6/main.py
+++ Result6/main.py
@@ -6,6 +6,7 @@ from Src.Logics.report_factory import report_factory
 from Src.Logics.start_factory import start_factory
 from datetime import datetime
 from Src.Logics.storage_service import storage_service
+from Src.Models.nomenclature_model import nomenclature_model
 
 
 app = Flask(__name__)
@@ -45,10 +46,10 @@ def get_turns():
     # Получить параметры
     args = request.args
     if "start_period" not in args.keys():
-        return error_proxy.create_error_response("Необходимо передать параметры: start_period, stop_period!")
+        return error_proxy.create_error_response(app, "Необходимо передать параметры: start_period, stop_period!")
         
     if "stop_period" not in args.keys():
-        return error_proxy.create_error_response("Необходимо передать параметры: start_period, stop_period!")
+        return error_proxy.create_error_response(app, "Необходимо передать параметры: start_period, stop_period!")
     
     start_date = datetime.strptime(args["start_period"], "%Y-%m-%d")
     stop_date = datetime.strptime(args["stop_period"], "%Y-%m-%d")
@@ -58,6 +59,34 @@ def get_turns():
     result = storage_service.create_response( data, app )
     return result
       
+      
+@app.route("/api/storage/<nomenclature_id>/turns", methods = ["GET"] )
+def get_turns_nomenclature(nomenclature_id):
+    
+    # Получить параметры
+    args = request.args
+    if "start_period" not in args.keys():
+        return error_proxy.create_error_response(app, "Необходимо передать параметры: start_period, stop_period!")
+
+    if "stop_period" not in args.keys():
+        return error_proxy.create_error_response(app, "Необходимо передать параметры: start_period, stop_period!")
+
+    start_date = datetime.strptime(args["start_period"], "%Y-%m-%d")
+    stop_date = datetime.strptime(args["stop_period"], "%Y-%m-%d")
+
+    transactions_data = start.storage.data[  storage.storage_transaction_key()   ]   
+    nomenclature_data =  start.storage.data[  storage.nomenclature_key()   ]   
+    
+    nomenclatures =  nomenclature_model.create_dictionary( nomenclature_data )
+    ids = [item.id for item in nomenclatures.values()]
+    if nomenclature_id not in ids:
+        return error_proxy.create_error_response(app, "Некорректно передан код номенклатуры!")
+    
+    nomenclature = nomenclatures[nomenclature_id]
+      
+    data = storage_service( transactions_data  ).create_turns_by_nomenclature( start_date, stop_date, nomenclature )      
+    result = storage_service.create_response( data, app )
+    return result      
 
 if __name__ == "__main__":
     app.run(debug = True)
