7ba59ee6d571712fd2109c6124ef39545af13466
Volovikov Alexander <trp2008@yandex.ru>
2024-05-18 13:24:26 +0300

    added samples

--- Sample2/csv_to_fasta.py
+++
@@ -1,9 +0,0 @@
-import csv
-seq_file = open('C:\Users\dell7\Documents\Tzachi\workspace\FitSeq\data\\reference_variant_full_sequences.csv','rb')
-seq_csv = csv.reader(seq_file)
-seq_fasta = open('C:\Users\dell7\Documents\Tzachi\workspace\FitSeq\data\\reference_variant_full_sequences.fa','wb')
-seq_csv.next()
-for seq in seq_csv:
-#    seq_fasta.write('>' + seq[0] + ' | Gene: ' + seq[1] + ' | CDS Type: ' + seq[2] + ' | Promoter: ' + seq[3] + ' | RBS: ' + seq[4] + '\n')
-    seq_fasta.write('>' + seq[0] + '\n')
-    seq_fasta.write(seq[1]+ '\n')
--- Sample2/readme.md
+++
@@ -1,3 +0,0 @@
-# Пример для рефакторинга
-Исходная ссылка: https://github.com/avivio/bio_utils/blob/master/csv_to_fasta.py
-
